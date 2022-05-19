03adc1aa43489814aee1fd96d3248c226db35295
Ramu <ramugadde84@gmail.com>
2022-05-19 17:01:39 +0530

    Removing Duplicates in string.

---
+++ test/examples/RemoveConsecutiveDuplicatesString.java
@@ -0,0 +1,21 @@
+package test.examples;
+
+public class RemoveConsecutiveDuplicatesString {
+
+
+    public static String removeConsecutiveDuplicates(String name) {
+        if (name.length() <= 1)
+            return name;
+        //
+        if (name.charAt(0) == name.charAt(1)) {
+            return removeConsecutiveDuplicates(name.substring(1));
+        } else {
+            return name.charAt(0) + removeConsecutiveDuplicates(name.substring(1));
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(removeConsecutiveDuplicates("raammmmuuu"));
+    }
+}

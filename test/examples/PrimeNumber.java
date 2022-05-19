437b0136b33068c6075d294b9938330a32d9cf5b
Ramu <ramugadde84@gmail.com>
2022-05-19 20:20:52 +0530

    Fibonacci series and prime numbers

--- test/examples/PrimeNumber.java
+++ test/examples/PrimeNumber.java
@@ -2,23 +2,25 @@ package test.examples;
 
 public class PrimeNumber {
 
-    public void primeNumbers(){
-        for(int i=2;i<30;i++){
-            int count=1;
-            for(int j=2;j<i;j++){
-                if(i%j ==0){
-                    count=0;
+    public static void primeNumbers() {
+        for (int i = 2; i < 30; i++) {
+            int count = 1;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    count = 0;
                     break;
                 }
 
-            }if(count==1){
+            }
+            if (count == 1) {
                 System.out.print(i + " ");
 
             }
         }
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        primeNumbers();
     }
 
 }
